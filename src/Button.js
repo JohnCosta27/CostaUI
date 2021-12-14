b039234b03252c668c74de55b8500c7e865b425d
John Costa <johncosta027@gmail.com>
2021-12-14 23:26:01 +0000

    Initial commit

---
+++ src/Button.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Button = () => {
+  return (
+    <button className="text-3xl bg-polar-night-400 text-snow-storm-100 w-48 rounded-md">
+      CostaUI
+    </button>
+  );
+};
+export default Button;
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Button from './Button';
+
+import './index.css';
+
+ReactDOM.render(<Button />, document.getElementById('root'));
